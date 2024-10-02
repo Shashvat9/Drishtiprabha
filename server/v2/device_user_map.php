9fea3caf4f4f4dcd49afc0cf45b55ab4f6324f92
Shahsvat rajyaguru <rajyagurushashvat@gmail.com>
2024-10-02 15:02:19 +0530

    Update device name when updating device ID in user_device_map table

--- server/v2/device_user_map.php
+++ server/v2/device_user_map.php
@@ -5,6 +5,7 @@
         $api_key = $_POST['api_key'];
         $d_id = $_POST['d_id'];
         $email = $_POST['email'];
+        $d_name = $_POST['d_name'];
 
         $api_key_value="dp123";
         
@@ -12,7 +13,14 @@
             $update_mapping_table = "UPDATE user_device_map SET d_id = '$d_id' WHERE email = '$email';";
             $update_mapping_table_fire = mysqli_query($con,$update_mapping_table);
             if($update_mapping_table_fire){
-                json_send(1,"Device ID updated successfully");
+                $update_device = "UPDATE device SET device_name = '$d_name' WHERE d_id = '$d_id';";
+                $update_device_fire = mysqli_query($con,$update_device);
+                if($update_device_fire){
+                    json_send(1,"Device ID updated successfully");
+                }
+                else{
+                    json_send(0,"Device ID updation failed");
+                }
             }else{
                 json_send(0,"Device ID updation failed");
             }
