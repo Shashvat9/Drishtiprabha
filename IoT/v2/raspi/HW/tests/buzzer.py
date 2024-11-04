5c50eae4500747c05d6f6286150fdfd3594bea0d
Shahsvat rajyaguru <rajyagurushashvat@gmail.com>
2024-11-04 15:53:12 +0530

    Refactor Buzzer class to include frequency parameter and use PWM for buzzing

--- IoT/v2/raspi/HW/tests/buzzer.py
+++ IoT/v2/raspi/HW/tests/buzzer.py
@@ -2,32 +2,27 @@ import RPi.GPIO as GPIO
 import time
 
 class Buzzer:
-    def __init__(self, pin=37, duration=1):
+    def __init__(self, pin=37, duration=1, frequency=1000):
         self.pin = pin
         self.duration = duration
+        self.frequency = frequency  # Frequency in Hertz
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.pin, GPIO.OUT)
-    
+        self.pwm = GPIO.PWM(self.pin, self.frequency)
+
     def buzz(self):
-        GPIO.output(self.pin, GPIO.HIGH)
+        self.pwm.start(50)  # Start PWM with 50% duty cycle
         time.sleep(self.duration)
-        GPIO.output(self.pin, GPIO.LOW)
-    
+        self.pwm.stop()
+
     def cleanup(self):
         GPIO.cleanup()
 
 if __name__ == "__main__":
-    buzzer = Buzzer(pin=37, duration=1)
-    
+    buzzer = Buzzer(pin=37, duration=1, frequency=1000)
     try:
         while True:
             buzzer.buzz()
             time.sleep(1)
     except KeyboardInterrupt:
         buzzer.cleanup()
-        
-            
-    # while True:
-    #     buzzer.buzz()
-        
-    # # buzzer.cleanup()
