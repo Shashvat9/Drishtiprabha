b0e2a6643d2f5c14914891aa3f0d5c8975d2a97d
Shahsvat rajyaguru <rajyagurushashvat@gmail.com>
2024-11-05 12:49:41 +0530

    Add main execution block to ultrasonic sensor for distance measurement

--- IoT/v2/raspi/HW/v2/utils/ultrasonic.py
+++ IoT/v2/raspi/HW/v2/utils/ultrasonic.py
@@ -44,3 +44,14 @@ class Ultrasonic:
 
     def cleanup(self):
         GPIO.cleanup()
+        
+if __name__ == "__main__":
+    ultrasonic_sensor = Ultrasonic(trig_pin=15, echo_pin=16)
+    try:
+        while True:
+            distance = ultrasonic_sensor.measure_distance()
+            print(f"Distance: {distance} cm")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        ultrasonic_sensor.cleanup()
+        print("Exiting")
