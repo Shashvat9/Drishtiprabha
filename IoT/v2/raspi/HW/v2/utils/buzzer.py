ae0a5349c2f278562dabe3d348e7c4c1e0014fa8
Shahsvat rajyaguru <rajyagurushashvat@gmail.com>
2024-11-05 13:45:04 +0530

    Refactor buzzer control logic and improve frequency setting based on distance

--- IoT/v2/raspi/HW/v2/utils/buzzer.py
+++ IoT/v2/raspi/HW/v2/utils/buzzer.py
@@ -10,45 +10,40 @@ class Buzzer:
         self.pwm = GPIO.PWM(self.pin, self.frequency)
         self.pwm.start(0)  # Start with buzzer off
 
-    def buzz_off(self):
-        self.pwm.ChangeDutyCycle(0) # 0% duty cycle to turn off
-        
-        
+    def set_frequency(self, frequency):
+        self.frequency = frequency
+        self.pwm.ChangeFrequency(self.frequency)
+
     def buzz_on(self, duration):
         self.pwm.ChangeDutyCycle(100)  # 100% duty cycle
         time.sleep(duration)
-        self.buzz_off()  
+        self.buzz_off()
 
+    def buzz_off(self):
+        self.pwm.ChangeDutyCycle(0)    # 0% duty cycle to turn off
 
     def buzz_control(self, distance):
-        # if 70 <= distance <= 100:
-        #     self.buzz_on()
-        #     time.sleep(0.35)
-        #     self.buzz_off()
-        # elif 40 <= distance < 70:
-        #     self.buzz_on()
-        #     time.sleep(0.25)
-        #     self.buzz_off()
-        # elif 10 <= distance < 40:
-        #     self.buzz_on()
-        #     time.sleep(0.15)
-        #     self.buzz_off()
-        # elif distance < 10:
-        #     self.buzz_on()
+        # if distance <= 10:
+        #     self.set_frequency(2000)
+        #     self.buzz_on(0.05)
+        # elif distance < 40:
+        #     self.set_frequency(1500)
+        #     self.buzz_on(0.15)
+        # elif distance < 70:
+        #     self.set_frequency(1000)
+        #     self.buzz_on(0.25)
+        # elif distance <= 100:
+        #     self.set_frequency(500)
+        #     self.buzz_on(0.35)
         # else:
         #     self.buzz_off()
-        # self.buzz_on()
-        # time.sleep(distance/100)
-        # self.buzz_off()
         
-        if(distance <= 100):
-            GPIO.PWM.ChangeFrequence(self.frequency-500)
-            self.buzz_on(distance/1000)
-        elif(distance < 50):
-            GPIO.PWM.ChangeFrequence(self.frequency)
+        if(distance<=100 and distance>50):
+            self.set_frequency(2000)
             self.buzz_on(distance/100)
-        else:
-            self.buzz_off()
+        elif(distance<=50):
+            self.set_frequency(1500)
+            self.buzz_on(distance/100)
 
     def cleanup(self):
         self.pwm.stop()
