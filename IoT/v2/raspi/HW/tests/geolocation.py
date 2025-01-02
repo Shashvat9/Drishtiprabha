2a69e8d2cad47b402cdc71ca661367941ffc4dfa
Shahsvat rajyaguru <rajyagurushashvat@gmail.com>
2025-01-02 18:48:44 +0530

    Refactor geolocation.py to use socket and geocoder for IP-based location retrieval; remove GPS-related code. Add initial gsm.cpp for SIM900 communication setup.

--- IoT/v2/raspi/HW/tests/geolocation.py
+++ IoT/v2/raspi/HW/tests/geolocation.py
@@ -1,58 +1,19 @@
-import serial
-import sys
-import webbrowser
+import socket
+import geocoder      
+      
+      
+def get_ip_address():
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
+    return IPAddr
+      
+g = geocoder.ip(get_ip_address())  # Replace "my_ip_address" with your actual IP            
+if g.ok:    
+    
+    print("Latitude:", g.lat)    
+    print("Longitude:", g.lng)    
+else:    
+    print("Error getting location data")
 
-# Define the function to convert raw NMEA string into degree decimal format
-def convert_to_degrees(raw_value):
-    decimal_value = raw_value / 100.00
-    degrees = int(decimal_value)
-    mm_mmmm = (decimal_value - int(decimal_value)) / 0.6
-    position = degrees + mm_mmmm
-    position = "%.4f" % (position)
-    return position
 
-# Define the function to get GPS info
-def GPS_Info():
-    global NMEA_buff
-    global lat_in_degrees
-    global long_in_degrees
 
-    nmea_time = NMEA_buff[0]  # Extract time from GPGGA string
-    nmea_latitude = NMEA_buff[1]  # Extract latitude from GPGGA string
-    nmea_longitude = NMEA_buff[3]  # Extract longitude from GPGGA string
-
-    print("NMEA Time: ", nmea_time, '\n')
-    print("NMEA Latitude:", nmea_latitude, "NMEA Longitude:", nmea_longitude, '\n')
-
-    lat = float(nmea_latitude)  # Convert string into float for calculation
-    longi = float(nmea_longitude)  # Convert string into float for calculation
-
-    lat_in_degrees = convert_to_degrees(lat)  # Get latitude in degree decimal format
-    long_in_degrees = convert_to_degrees(longi)  # Get longitude in degree decimal format
-
-# Initialize global variables
-gpgga_info = "$GPGGA,"
-ser = serial.Serial("/dev/ttyS0")  # Open port with baud rate
-GPGGA_buffer = 0
-NMEA_buff = 0
-lat_in_degrees = 0
-long_in_degrees = 0
-
-try:
-    while True:
-        received_data = str(ser.readline())  # Read NMEA string received
-        GPGGA_data_available = received_data.find(gpgga_info)  # Check for NMEA GPGGA string
-
-        if GPGGA_data_available > 0:
-            GPGGA_buffer = received_data.split("$GPGGA,", 1)[1]  # Store data coming after "$GPGGA," string
-            NMEA_buff = GPGGA_buffer.split(',')  # Store comma-separated data in buffer
-            GPS_Info()  # Get time, latitude, longitude
-
-            print("lat in degrees:", lat_in_degrees, " long in degree: ", long_in_degrees, '\n')
-            map_link = 'http://maps.google.com/?q=' + str(lat_in_degrees) + ',' + str(long_in_degrees)  # Create a link to plot location on Google map
-            print("<<<<<<<>>>>>\n")  # Press ctrl+c to plot on the map and exit
-            print("------------------------------------------------------------\n")
-
-except KeyboardInterrupt:
-    webbrowser.open(map_link)  # Open the current position information in Google Maps
-    sys.exit(0)
